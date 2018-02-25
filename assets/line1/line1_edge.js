ad5bc92b5c39bed86b6f212d469f9e00c24fecb7
EBeginnings <31580207+EBeginnings@users.noreply.github.com>
2018-02-25 16:44:35 -0500

    Add files via upload

---
+++ assets/line1/line1_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',e6='${Rectangle}',x3='6.0.0.400',x2='5.0.0',lf='left',x4='rgba(0,0,0,1.00)',m='rect',w='width',xc='rgba(0,0,0,1)',x5='rgba(255,255,255,0.00)',i='none';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Rectangle',t:m,r:['0px','4px','0px','1px','auto','auto'],f:[x4],s:[0,xc,i]}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'140px','10px'],overflow:'hidden',f:[x5]}}},tt:{d:500,a:n,data:[["eid7",lf,0,500,"linear",e6,'0px','8px'],["eid2",w,0,500,"linear",e6,'0px','132px']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-52782196");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-52782196");
---
+++ assets/line2/line2_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',e6='${Rectangle}',x3='6.0.0.400',x2='5.0.0',x4='rgba(0,0,0,1.00)',tp='top',m='rect',bt='bottom',h='height',xc='rgba(0,0,0,1)',x5='rgba(255,255,255,0.00)',i='none';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Rectangle',t:m,r:['10px','auto','1px','0px','auto','13px'],f:[x4],s:[0,xc,i]}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'20px','186px'],overflow:'hidden',f:[x5]}}},tt:{d:500,a:n,data:[["eid8",tp,0,500,"linear",e6,'173px','auto'],["eid7",h,0,500,"linear",e6,'0px','132px'],["eid9",bt,0,500,"linear",e6,'13px','48px']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-80295260");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-80295260");
